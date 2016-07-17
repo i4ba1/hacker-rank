7c4160f80a5c74d143a8a520aba7f157ec8ca078
administrator <administrator>
2016-07-17 16:04:02 +0700

    First commit hacker rank project ini GitHub

---
+++ src/com/code/rank/FindMissingNumbers.java
@@ -0,0 +1,19 @@
+package com.code.rank;
+
+/**
+ * Created by Administrator on 7/14/2016.
+ */
+public class FindMissingNumbers {
+    public static void main(String[] args){
+        int a[] = {1,2,4,5,6};
+        int n = a.length;
+        int total = (n+1)*(n+2)/2;
+        int count = 0;
+
+        for (int i=0; i<n; i++){
+            total -= a[i];
+        }
+
+        System.out.println(total);
+    }
+}
---
+++ src/com/code/rank/MissingNumbers.java
@@ -0,0 +1,10 @@
+package com.code.rank;
+
+/**
+ * Created by Administrator on 7/15/2016.
+ */
+public class MissingNumbers {
+    public static void main(String args){
+
+    }
+}
